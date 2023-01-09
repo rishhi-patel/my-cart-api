c8968d5c75d28fc4962a505ca1a0905430720024
rishi <rishi.patel@dignizant.com>
2023-01-10 01:36:44 +0530

    --added default path

--- index.js
+++ index.js
@@ -25,6 +25,10 @@ if (process.env.NODE_ENV === 'development') {
 app.use(cors());
 app.use(express.json())
 
+app.get("/", (req, res) => {
+  res.send("API is running....");
+});
+
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/orders', orderRoutes)
