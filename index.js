e9b1e4741a32a860dd6fbfafc0d343c7f96a956e
rishi <rishi.patel@dignizant.com>
2023-01-09 23:44:06 +0530

    Fix : CORS Error

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const colors = require('colors')
 const morgan = require('morgan')
 const { notFound, errorHandler } = require('./middleware/errorMiddleware.js')
 const connectDB = require('./config/db.js')
+const cors = require("cors");
 
 const productRoutes = require('./routes/productRoutes.js')
 const userRoutes = require('./routes/userRoutes.js')
@@ -21,6 +22,7 @@ if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'))
 }
 
+app.use(cors());
 app.use(express.json())
 
 app.use('/api/products', productRoutes)
